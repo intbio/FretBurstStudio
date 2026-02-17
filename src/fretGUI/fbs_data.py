cf64af76e9d1b5a81ad4328278831f5b430b4228
d-ryabov-biomsu <d.ryabov@intbio.org>
2026-02-17 14:59:34 +0300

    bugs fixed

--- src/fretGUI/fbs_data.py
+++ src/fretGUI/fbs_data.py
@@ -4,12 +4,12 @@ from singletons import FBSDataIDGenerator
 
 
 class FBSData():
-    def __init__(self, data=None, path=None, id=None, checked=False):
+    def __init__(self, data=None, path=None, id=None, checked=False, node_metadata=None):
         self.__data = data if data else Data()
         self.__path = path if path else ''
         self.__id = id if id else FBSDataIDGenerator().get_next_id()
         self.__checked = checked
-        self.__node_metadata = []
+        self.__node_metadata = node_metadata if node_metadata else []
         
     def add_node_metadata(self, metadata: dict):
         self.__node_metadata.append(metadata)
@@ -46,7 +46,7 @@ class FBSData():
         return self.__id
         
     def copy(self):
-        new_obj = FBSData(deepcopy(self.__data), self.__path, id=self.__id, checked=self.__checked, __node_metadata=self.node_metadata)
+        new_obj = FBSData(deepcopy(self.__data), self.__path, id=self.__id, checked=self.__checked, node_metadata=self.node_metadata)
         return new_obj
     
     def __repr__(self):
